dff9234e96cf32342fd0d8d2f4d7fecb83dad204
Tiago Gimenes <tlgimenes@gmail.com>
2020-07-17 12:23:38 -0300

    adds crossorigin

--- gatsby.ssr.js
+++ gatsby.ssr.js
@@ -6,11 +6,13 @@ exports.onRenderBody = ({
       key='preconnect-googletagmanager'
       rel='preconnect'
       href='https://www.googletagmanager.com/'
+      crossOrigin="true"
     />,
     <link
       key='preconnect-google-analytics'
       rel='preconnect'
       href='https://www.google-analytics.com/'
+      crossOrigin="true"
     />
   ])
 }
