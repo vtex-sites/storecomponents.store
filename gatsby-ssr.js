49c70056550be315b530bd54813edb428cc20ac4
Tiago Gimenes <tlgimenes@gmail.com>
2020-07-17 12:53:07 -0300

    remove preconnect

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,11 +4,11 @@ export const onRenderBody = ({
   setHeadComponents,
 }) => {
   setHeadComponents([
-    <link
-      key='preconnect-google-analytics'
-      rel='preconnect'
-      href='https://www.google-analytics.com/'
-      crossOrigin="true"
-    />
+    // <link
+    //   key='preconnect-google-analytics'
+    //   rel='preconnect'
+    //   href='https://www.google-analytics.com/'
+    //   crossOrigin="true"
+    // />
   ])
 }
