839171f999c393f602771a570508828d5a158aac
Tiago Gimenes <tlgimenes@gmail.com>
2020-07-17 12:39:39 -0300

    rename files

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,12 +3,6 @@ exports.onRenderBody = ({
 }) => {
   setHeadComponents([
     <link
-      key='preconnect-googletagmanager'
-      rel='preconnect'
-      href='https://www.googletagmanager.com/'
-      crossOrigin="true"
-    />,
-    <link
       key='preconnect-google-analytics'
       rel='preconnect'
       href='https://www.google-analytics.com/'
