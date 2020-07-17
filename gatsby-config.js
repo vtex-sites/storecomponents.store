fae1fa35b4c154dbf684c160f443198f27a80cc9
Tiago Gimenes <tlgimenes@gmail.com>
2020-07-17 14:43:02 -0300

    marketing platform

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,45 +33,45 @@ module.exports = {
         path: './src/cms/',
       },
     },
-    // {
-    //   resolve: require.resolve('gatsby-plugin-google-marketing-platform'),
-    //   options: {
-    //     tagmanager: {
-    //       id: 'GTM-TKDFD35'
-    //     },
-    //     analytics: {
-    //       id: 'UA-43760863-27'
-    //     }
-    //   },
-    // },
     {
-      resolve: "gatsby-plugin-google-tagmanager",
+      resolve: require.resolve('gatsby-plugin-google-marketing-platform'),
       options: {
-        id: "GTM-TKDFD35",
-
-        // Include GTM in development.
-        //
-        // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: false,
-
-        // datalayer to be set before GTM is loaded
-        // should be an object or a function that is executed in the browser
-        //
-        // Defaults to null
-        defaultDataLayer: { platform: "gatsby" },
-
-        // Specify optional GTM environment details.
-        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-        // dataLayerName: "YOUR_DATA_LAYER_NAME",
-
-        // Name of the event that is triggered
-        // on every Gatsby route change.
-        //
-        // Defaults to gatsby-route-change
-        // routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
+        tagmanager: {
+          id: 'GTM-TKDFD35'
+        },
+        analytics: {
+          id: 'UA-43760863-27'
+        }
       },
     },
+    // {
+    //   resolve: "gatsby-plugin-google-tagmanager",
+    //   options: {
+    //     id: "GTM-TKDFD35",
+
+    //     // Include GTM in development.
+    //     //
+    //     // Defaults to false meaning GTM will only be loaded in production.
+    //     includeInDevelopment: false,
+
+    //     // datalayer to be set before GTM is loaded
+    //     // should be an object or a function that is executed in the browser
+    //     //
+    //     // Defaults to null
+    //     defaultDataLayer: { platform: "gatsby" },
+
+    //     // Specify optional GTM environment details.
+    //     // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+    //     // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+    //     // dataLayerName: "YOUR_DATA_LAYER_NAME",
+
+    //     // Name of the event that is triggered
+    //     // on every Gatsby route change.
+    //     //
+    //     // Defaults to gatsby-route-change
+    //     // routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
+    //   },
+    // },
     // {
     //   resolve: `gatsby-plugin-google-analytics`,
     //   options: {
