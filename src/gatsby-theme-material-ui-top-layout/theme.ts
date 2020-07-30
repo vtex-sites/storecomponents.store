99fd593f83e063c09380f51008fb4744046b992d
Estácio Pereira <estacio.pereira@gmail.com>
2020-07-30 16:12:43 -0300

    Create Material UI PoC from updated master

---
+++ src/gatsby-theme-material-ui-top-layout/theme.ts
@@ -0,0 +1,41 @@
+import { createMuiTheme } from '@material-ui/core'
+
+const theme = createMuiTheme({
+  // Define custom style for HighLights Component
+  // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+  // @ts-ignore
+  // HighlightsRoot: {
+  // backgroundColor: '#0f3e99',
+  // paddingBottom: 14,
+  // paddingTop: 14,
+  // },
+  palette: {
+    primary: {
+      main: '#0f3e99',
+    },
+  },
+  overrides: {
+    MuiCard: {
+      root: {
+        boxShadow: 'none',
+      },
+    },
+    MuiButton: {
+      root: {
+        backgroundColor: '#0f3e99',
+        color: 'white',
+        textTransform: 'none',
+        '&:hover': {
+          backgroundColor: '#0f3e99',
+        },
+      },
+    },
+    MuiLink: {
+      root: {
+        color: 'inherit',
+      },
+    },
+  },
+})
+
+export default theme
