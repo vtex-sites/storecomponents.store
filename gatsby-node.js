4f73a64182bb345d67b6c289831e00d8179b28b7
Tiago Gimenes <tlgimenes@gmail.com>
2020-10-07 22:31:23 -0300

    adding files

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,3 +8,17 @@ exports.onCreateWebpackConfig = ({ actions: { setWebpackConfig } }) => {
     },
   })
 }
+
+exports.createPages = ({ actions: { createRedirect } }) => {
+  createRedirect({
+    fromPath: '/checkout/*',
+    toPath: `https://storetheme.vtex.com/checkout/:splat`,
+    statusCode: 200,
+  })
+
+  createRedirect({
+    fromPath: '/_v/private/auth-server/v1/login',
+    toPath: `https://storetheme.vtex.com/_v/private/auth-server/v1/login`,
+    statusCode: 200,
+  })
+}
