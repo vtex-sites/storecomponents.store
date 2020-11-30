a49b730e4a46db6a33728f334a9861ec551574a2
Tiago Gimenes <tlgimenes@gmail.com>
2020-11-30 12:03:40 -0300

    add shadowing

---
+++ src/@vtex/gatsby-plugin-cms/index.ts
@@ -0,0 +1,7 @@
+import { Components, Schemas, ContentTypes } from '@vtex/gatsby-plugin-cms'
+
+export const components: Components = {}
+
+export const schemas: Schemas = {}
+
+export const contentTypes: ContentTypes = {}
