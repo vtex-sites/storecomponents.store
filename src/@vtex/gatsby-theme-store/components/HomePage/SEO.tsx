edc651a9391422bdd2bf2c1caa1ec2302d87a55a
Tiago Gimenes <tlgimenes@gmail.com>
2021-01-08 14:09:14 -0300

    Add new CMS SEO schema and use it into the home SEO

---
+++ src/@vtex/gatsby-theme-store/components/HomePage/SEO.tsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import Helmet from '@vtex/gatsby-theme-store/src/components/SEO/Helmet'
+import { graphql, PageProps, useStaticQuery } from 'gatsby'
+import type { FC } from 'react'
+
+import type { HomePageQueryQuery } from '../../pages/__generated__/HomePageQuery.graphql'
+import { HomePageSeoQueryQuery } from './__generated__/HomePageSEOQuery.graphql'
+
+type Props = PageProps<HomePageQueryQuery>
+
+const SEO: FC<Props> = ({ data: { vtexCmsPageContent } }) => {
+  const [
+    { props: siteMetadata },
+    { props: facebook },
+  ] = vtexCmsPageContent!.extraBlocks[0]!.blocks!
+
+  const { site } = useStaticQuery<HomePageSeoQueryQuery>(
+    graphql`
+      query HomePageSEOQuery {
+        site {
+          siteMetadata {
+            siteUrl
+          }
+        }
+      }
+    `
+  )
+
+  return (
+    <Helmet
+      title={siteMetadata.title}
+      meta={[
+        {
+          name: 'description',
+          content: siteMetadata.description,
+        },
+        {
+          property: 'og:url',
+          content: site?.siteMetadata?.siteUrl,
+        },
+        {
+          property: 'og:type',
+          content: 'website',
+        },
+        {
+          property: 'og:title',
+          content: facebook.title,
+        },
+        {
+          property: 'og:description',
+          content: facebook.description,
+        },
+        {
+          property: 'og:image',
+          content: facebook.thumbnail,
+        },
+      ]}
+    />
+  )
+}
+
+export default SEO
