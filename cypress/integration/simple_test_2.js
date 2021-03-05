d439e21bfe929515ad736db41c55332ff1198dc1
Thiago Veras <thiago.veras.machado@gmail.com>
2021-03-05 12:57:52 -0300

    Test

--- cypress/integration/simple_test_2.js
+++ cypress/integration/simple_test_2.js
@@ -9,7 +9,7 @@ describe('File test 2 - 1', () => {
         expect(true).to.equal(true)
     })
     it('File: 2 (4) Break', () => {
-        // import x from 'blabla'
+        import x from 'blabla'
         expect(true).to.equal(true)
     })
     it('File: 2 (5)', () => {
