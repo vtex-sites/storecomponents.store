370ae75a0a4a6e76a78ccb20b35281c08f456d56
Rodrigo Orem <orem.rodrigo@gmail.com>
2021-04-01 19:11:04 -0300

    Trigger build

--- api/hello.js
+++ api/hello.js
@@ -4,7 +4,6 @@ exports.handler = async (event) => {
   if (event.queryStringParameters && event.queryStringParameters.name) {
     name = event.queryStringParameters.name
   }
-
   return {
     statusCode: 200,
     body: `Hello ${name}`,
