f552cec1ddceab98cce6395be710b8293a275bb3
Rodrigo Orem <orem.rodrigo@gmail.com>
2021-04-06 10:51:56 -0300

    Add function

---
+++ api/hello.js
@@ -0,0 +1,12 @@
+exports.handler = async (event) => {
+  let name = 'World'
+
+  if (event.queryStringParameters && event.queryStringParameters.name) {
+    name = event.queryStringParameters.name
+  }
+
+  return {
+    statusCode: 200,
+    body: `Hello ${name}`,
+  }
+}
