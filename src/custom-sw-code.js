e2a42ee96c3825ef9329cde5e599075d74f85e8f
victorhmp <victor2142@gmail.com>
2021-04-07 12:02:32 -0300

    Add PWA configuration and custom SW code for offline support

---
+++ src/custom-sw-code.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-restricted-globals */
+const OFFLINE_PAGE_CACHE_NAME = 'offline-page'
+const OFFLINE_PAGE = '/_v/offline'
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(OFFLINE_PAGE_CACHE_NAME)
+
+      // Setting {cache: 'reload'} in the new request will ensure that the response
+      // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
+      await cache.add(new Request(OFFLINE_PAGE, { cache: 'reload' }))
+      // 👆🏻 Adding the offline page to the cache
+    })()
+  )
+})
+
+// This is gonna handle failed network requests.
+// We're simply returning a cached offline page here.
+const catchHandler = async ({ event }) => {
+  const dest = event.request.destination
+
+  if (dest === 'document') {
+    // Getting the offline page from cache.
+    const cache = await caches.open(OFFLINE_PAGE_CACHE_NAME)
+    const cachedResponse = await cache.match(OFFLINE_PAGE)
+
+    return cachedResponse
+  }
+
+  return Response.error()
+}
+
+// `workbox` will be defined in the scope where this code is executed.
+// eslint-disable-next-line no-undef
+workbox.routing.setCatchHandler(catchHandler)
