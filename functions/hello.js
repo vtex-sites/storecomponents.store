57bc402a770acf1a0b72abe0c18d31f84ba97305
Rodrigo Orem <orem.rodrigo@gmail.com>
2021-04-15 05:07:24 -0300

    Trigger build

--- functions/hello.js
+++ functions/hello.js
@@ -1,5 +1,5 @@
 exports.handler = async (event) => {
-  let name = 'World!!!!!!!!!!!!!!!!!'
+  let name = 'World!!!!!!!!!!!!!!!!!!'
 
   if (event.queryStringParameters && event.queryStringParameters.name) {
     name = event.queryStringParameters.name
