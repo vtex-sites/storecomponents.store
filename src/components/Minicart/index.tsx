d2912ab0a409fb1df0369514df7961b11ff74059
Tiago Gimenes <tlgimenes@gmail.com>
2021-04-22 15:29:42 -0300

    fix sonarqube

--- src/components/Minicart/index.tsx
+++ src/components/Minicart/index.tsx
@@ -4,10 +4,8 @@ import type { FC } from 'react'
 
 const MinicartClient = lazy(() => import('./Minicart.client'))
 
-const noop = () => {}
-
 const Minicart: FC = () => (
-  <SuspenseSSR fallback={<MinicartButton value={0} onClick={noop} />}>
+  <SuspenseSSR fallback={<MinicartButton value={0} />}>
     <MinicartClient />
   </SuspenseSSR>
 )
