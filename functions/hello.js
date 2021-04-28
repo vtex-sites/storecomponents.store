dfcf6d9069d43925daccc4c474d9bb5d73f1f06b
Rodrigo Orem <orem.rodrigo@gmail.com>
2021-04-28 17:20:55 -0300

    Trigger build

---
+++ functions/hello.js
@@ -0,0 +1,12 @@
+exports.handler = async (event) => {
+  let name = '@channel!!!!!!!!!!'
+
+  if (event.queryStringParameters && event.queryStringParameters.name) {
+    name = event.queryStringParameters.name
+  }
+
+  return {
+    statusCode: 200,
+    body: `Hi ${name}`,
+  }
+}
