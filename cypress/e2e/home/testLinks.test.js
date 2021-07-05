731a073790d5f05d8abe68a0e7c9821b9ec5dffe
Bento Pereira <bento.pereira@vtex.com>
2021-07-05 11:58:52 -0300

    feat: scripts and test file

---
+++ cypress/e2e/home/testLinks.test.js
@@ -0,0 +1,35 @@
+describe('check all links in the home', () => {
+  before(() => {
+    cy.visit('/')
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(1000)
+  })
+  it('check all links to sites', () => {
+    const localURL = new URL(Cypress.config().baseUrl)
+
+    cy.get("a:not([href*='mailto:'])").each(($page) => {
+      if ($page.prop('href') === Cypress.config().baseUrl) {
+        return
+      }
+
+      const url = new URL($page.prop('href'))
+
+      if (url.hostname !== localURL.hostname) {
+        return
+      }
+
+      cy.intercept('GET', `/page-data${url.pathname}/page-data.json`).as(
+        'pageLoad'
+      )
+      cy.visit($page.prop('href'), { failOnStatusCode: false })
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      cy.wait('@pageLoad')
+        .its('response.statusCode')
+        .then(($code) => {
+          if ($code < 200 || $code > 399) {
+            cy.log(`Unexpected response code ${$code} for path ${url.pathname}`)
+          }
+        })
+    })
+  })
+})
